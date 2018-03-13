290eec7be93d81c1f2088a146867dcb0ca27730a
Bùi Văn Xứng UIT <xungbv.uit@gmail.com>
2018-03-13 23:40:03 +0700

    data

--- server.js
+++ server.js
@@ -14,13 +14,16 @@ function delay(ms) {
    while (new Date() < ms){}
 }
 function getNextSequenceValue(sequenceName){
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
         var sequenceDocument = db.counters.findAndModify({
         query:{_id: sequenceName },
         update: {$inc:{sequence_value:1}},
         new:true
           });
       return sequenceDocument.sequence_value;
-      }
+    });
+  }
 
 function InsertToDatabase(data, gio, ngay) {
   MongoClient.connect(url, function(err, db) {
